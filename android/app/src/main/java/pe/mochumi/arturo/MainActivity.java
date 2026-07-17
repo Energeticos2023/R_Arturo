4942502ce2b09dd76519d430e6abd0803820860c
Club Rotary Huaraz Colonial <jose.zena.p@uni.pe>
2026-07-16 23:34:43 -0500

    Add Android APK wrapper

---
+++ android/app/src/main/java/pe/mochumi/arturo/MainActivity.java
@@ -0,0 +1,33 @@
+package pe.mochumi.arturo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setSupportZoom(false);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://energeticos2023.github.io/R_Arturo/");
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
